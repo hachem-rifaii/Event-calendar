2b528e3804be72de19801754dfaa17b85141f74f
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-02 23:51:10 +0200

    make register page

--- backend/app.ts
+++ backend/app.ts
@@ -14,11 +14,11 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(
-  cors({
-    origin: process.env.ORIGIN,
-  })
-);
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true, 
+}));
 
 // routes
 app.use("/api/users" , userRouter)
