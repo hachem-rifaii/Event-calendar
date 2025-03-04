42d00a0d06694741457f2884d24282c8714c5e0f
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-04 21:42:37 +0200

    fixing

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,16 +1,28 @@
 import mongoose from "mongoose";
 require("dotenv").config();
+
 const dbURL: string = process.env.DB_URL || "";
 
 const connectDB = async () => {
+  if (!dbURL) {
+    console.error("Database URL is not defined!");
+    return;
+  }
+
+  console.log("Connecting to the database...");
+
   try {
-    await mongoose.connect(dbURL).then((data: any) => {
-      console.log(`Connected to the database ${data.connection.host}`);
+    await mongoose.connect(dbURL, {
+      connectTimeoutMS: 10000, 
     });
-  } catch (error : any) {
-    console.error(`Failed to connect to the database ${error.message} `);
+
+    console.log("Connected to the database successfully!");
+
+  } catch (error: any) {
+    console.error(`Failed to connect to the database: ${error.message}`);
     
-    setTimeout(connectDB,5000)
+    // إعادة المحاولة بعد 5 ثواني
+    setTimeout(connectDB, 5000);
   }
 };
 
