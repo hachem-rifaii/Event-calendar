cb4a0f601f92784152e1bcdf9a60fe6b617ad828
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-04 22:29:24 +0200

    fixing

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,10 @@
 import app  from './app';
 import dotenv from 'dotenv';
-
+import connectDB from './src/config/db';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
