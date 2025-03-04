cb7ef01273c7698fc4e98b093f7bddbd9d66169f
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-04 22:14:43 +0200

    fixing

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 require("dotenv").config();
-const dbURL: string =
-  "mongodb+srv://hachemrifai9:9zkumLT81X4ef11n@cluster0.zhl0g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbURL: string = process.env.DB_URL || "";
+
 const connectDB = async () => {
-  mongoose
-    .connect(dbURL, {
-      useNewUrlParser : true,
-      useUnifiedTopology: true,
-    })
-    .then((data) => {
-      console.log(`mongodb connected with server: ${data.connection.host}`);
+  try {
+    await mongoose.connect(dbURL).then((data: any) => {
+      console.log(`Connected to the database ${data.connection.host}`);
     });
+  } catch (error : any) {
+    console.error(`Failed to connect to the database ${error.message} `);
+    
+    setTimeout(connectDB,5000)
+  }
 };
 
 export default connectDB;
