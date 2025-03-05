2f9031e999db4edd7149eac0c4408f4326d39fb9
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-05 16:00:41 +0200

    fixing

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -2,7 +2,6 @@ require("dotenv").config();
 import { Response } from "express";
 import { IUser } from "../model/user.model";
 
-
 interface ITokenOptions {
   expires: Date;
   maxAge: number;
@@ -20,24 +19,23 @@ export const refreshTokenExpire = parseInt(
 );
 export const accessTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
-  maxAge: accessTokenExpire  * 60 * 60 * 1000,
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none",
-  secure:true
 };
 export const refreshTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
-  maxAge: refreshTokenExpire  * 24 * 60 * 60 * 1000,
+  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none",
-  secure:true
 };
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const accessToken = user.signAccessToken();
   const refreshToken = user.signRefreshToken();
 
-  if(process.env.NODE_ENV === 'production'){
+  if (process.env.NODE_ENV === "production") {
     accessTokenOptions.secure = true;
+    refreshTokenOptions.secure = true;
   }
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
