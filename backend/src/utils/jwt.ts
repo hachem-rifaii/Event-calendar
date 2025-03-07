255a10a9f314d5158e22fc7f6feebf10267baa59
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2025-03-07 02:45:05 +0200

    ---

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -9,6 +9,7 @@ interface ITokenOptions {
   sameSite: "lax" | "strict" | "none" | undefined;
   secure?: boolean;
   path : string
+  
 }
 export const accessTokenExpire = parseInt(
   process.env.ACCESS_TOKEN_EXPIRATION || "300",
@@ -23,14 +24,16 @@ export const accessTokenOptions: ITokenOptions = {
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none",
-  path : '/'
+  path : '/',
+  secure :true
 };
 export const refreshTokenOptions: ITokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "none",
-  path : '/'
+  path : '/',
+  secure : true
 
 };
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
@@ -38,10 +41,7 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const refreshToken = user.signRefreshToken();
   console.log("go to send token"); 
   
-  if (process.env.NODE_ENV === "production") {
-    accessTokenOptions.secure = true;
-    refreshTokenOptions.secure = true;
-  }
+
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
   console.log("sending the token");
